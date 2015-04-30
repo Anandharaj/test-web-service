838f9a683d12d9902397407b08004d7209c76cbc
Anandharaj <anandharajan777@gmail.com>
2015-04-30 16:54:02 +0530

    table design for kot

--- student_controller.php
+++ student_controller.php
@@ -1,9 +1,9 @@
 <?php
-	echo $_SERVER['REQUEST_METHOD'];
-	parse_str(file_get_contents("php://input"),$put_vars);
-	// echo $put_vars['type'];
-	var_dump($put_vars);
-	return;
+	// echo $_SERVER['REQUEST_METHOD'];
+	// parse_str(file_get_contents("php://input"),$put_vars);
+	// // echo $put_vars['type'];
+	// var_dump($put_vars);
+	// return;
 
 	include('student_service.php');
 
@@ -11,13 +11,18 @@
 	
 	switch ($_SERVER['REQUEST_METHOD']) {
 		case 'GET':
-			if (!$_GET["student_id"]) {
-				die('Parameter id is missing');
+			if (!isset($_GET["student_id"]) && !isset($_GET["type"])) {
+				die('Parameters is missing');
 			}
 
 			switch ($_GET['type']) {
 				case 'getStudentData':
-					$result = $instance->getStudentData($_GET['student_id']);
+					$result = null;
+					if (isset($_GET['student_id'])) {
+						$result = $instance->getStudentData($_GET['student_id']);
+					} else {
+						$result = $instance->getStudentData(null);
+					}
 					if (!$result) {
 						die('Record not found..!');
 					}
@@ -72,32 +77,32 @@
 			break;
 	}
 
-	if (!isset($_REQUEST['type'])) {
-		die("Unknown Request");
-		return;
-	}
+	// if (!isset($_REQUEST['type'])) {
+	// 	die("Unknown Request");
+	// 	return;
+	// }
 
-	if ($_REQUEST['type'] == "studentData") {
+	// if ($_REQUEST['type'] == "studentData") {
 		
-		// GET
-		if ($_SERVER['REQUEST_METHOD'] == "GET") {
-			if (isset($_GET['method']) && $_GET['method'] == "delete") {
-				//  delete opt
-			}
+	// 	// GET
+	// 	if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	// 		if (isset($_GET['method']) && $_GET['method'] == "delete") {
+	// 			//  delete opt
+	// 		}
 
-			if (isset($_GET['id'])) {
-				// get details for id
-				return;
-			}
+	// 		if (isset($_GET['id'])) {
+	// 			// get details for id
+	// 			return;
+	// 		}
 
-			// get all details
+	// 		// get all details
 
-		}
+	// 	}
 
-		// POST
-		if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['data'])) {
+	// 	// POST
+	// 	if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['data'])) {
 
-		}
-	}
+	// 	}
+	// }
 
 ?>
